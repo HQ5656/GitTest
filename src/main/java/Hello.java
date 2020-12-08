904740307f52c3eea3e47574998e6237d899033b
hq5656 <565638612@qq.com>
2020-12-08 21:25:59 +0800

    Merge branch 'master' of E:\IdeaProjects\GitTest with conflicts.

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
         System.out.println("111");
         System.out.println("3333");
         System.out.println("fen");
+        System.out.println("6");
         System.out.println("667");
         System.out.println("688");
     }
