cde1a7b74b2034e6a898db9f8d05588b1692aa52
hq5656 <565638612@qq.com>
2020-12-15 23:35:10 +0800

    test localDev

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -9,11 +9,13 @@ public class Hello {
         System.out.println("6");
         System.out.println("667");
         System.out.println("678");
+        System.out.println("1test2.1");
         System.out.println("111");
         System.out.println("1test1");
         System.out.println("1test");
         System.out.println("hello world");
-        System.out.println("123.");
+        System.out.println("1233.");
         System.out.println("1test2.");
+        System.out.println("1test2.1");
     }
 }
